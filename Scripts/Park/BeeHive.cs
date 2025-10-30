cb055bb3d94fdf5ff76f0bba95f9714f81e116ba
Abraão Melo <abraao.st.melo@gmail.com>
2025-10-29 23:15:19 -0300

    Shrink effect to hive

---
+++ Scripts/Park/BeeHive.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using UnityEngine;
+
+public class BeeHive : MonoBehaviour
+{
+    [Header("Shrink Settings")]
+    public string targetTag = "Player";
+    public float shrinkSpeed = 2f;     
+    public float minScale = 0.2f;     
+    public float moveSpeed = 3f;      
+    public Transform hiveHole;        
+    private GameObject player;
+    private bool isShrinking = false;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag(targetTag);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            GameObject joystick = GameObject.FindGameObjectWithTag("Joystick");
+            if (joystick != null) joystick.SetActive(false);
+
+            StartCoroutine(MoveAndShrinkToHiveHole());
+        }
+    }
+
+    private IEnumerator MoveAndShrinkToHiveHole()
+    {
+        isShrinking = true;
+        Vector3 targetScale = Vector3.one * minScale;
+
+        //Deactivate physics, need to control the object mannually.
+        Rigidbody2D rb = player.GetComponent<Rigidbody2D>();
+        if (rb != null)
+        {
+            rb.velocity = Vector2.zero;
+            rb.isKinematic = true;
+        }
+
+        while (isShrinking && player != null && hiveHole != null)
+        {
+            player.transform.position = Vector3.MoveTowards(
+                player.transform.position,
+                hiveHole.position,
+                moveSpeed * Time.deltaTime
+            );
+
+            player.transform.localScale = Vector3.MoveTowards(
+                player.transform.localScale,
+                targetScale,
+                shrinkSpeed * Time.deltaTime
+            );
+
+            float distance = Vector3.Distance(player.transform.position, hiveHole.position);
+            if (distance < 0.05f)
+            {
+                player.transform.position = hiveHole.position;
+                player.transform.localScale = targetScale;
+                isShrinking = false;
+
+                SceneFader.Instance.FadeToScene("Hive");
+            }
+
+            yield return null;
+        }
+    }
+}
